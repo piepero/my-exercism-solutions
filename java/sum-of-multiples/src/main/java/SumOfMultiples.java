b19cda158c21f13caf5f6d8bd7b4d69c1f2435d4
piepero <pieper@viaregio.de>
2018-02-18 21:04:55 +0100

    sum of multiples: first submitted solution

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,11 +1,28 @@
 class SumOfMultiples {
 
+    private int sumOfMultiples;
+
     SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.sumOfMultiples = 0;
+
+        for (int i = 0; i < number; i ++) {
+            if (isMultipleOf(i, set)) {
+                this.sumOfMultiples += i;
+            }
+        }
+    }
+
+    boolean isMultipleOf(int number, int[] set) {
+        for (int i = 0; i < set.length; i++) {
+            if (number % set[i] == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return sumOfMultiples;
     }
 
 }
